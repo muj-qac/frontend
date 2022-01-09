13823683762ef6c5cc7defbd842075ce0b2c4692
RAz <snehalraj85@gmail.com>
2022-01-09 20:08:34 +0530

    fix

--- src/pages/kpi.js
+++ src/pages/kpi.js
@@ -8,8 +8,8 @@ function Kpi() {
   const [showModal, setShowModal] = useState(false);
   return (
     <>
-      <div className=" grid grid-cols-12">
-        <div className=" col-start-1 col-span-2 justify-self-stretch">
+      <div className=" grid grid-cols-12 grid-flow-col">
+        <div className=" col-start-1 col-span-2 justify-self-stretch ">
           <SideBar />
         </div>
         {/* <div className=" col-start-7 h-10">
@@ -17,10 +17,10 @@ function Kpi() {
         Button
         </button>
       </div> */}
-        <div className="col-start-3">
+        <div className="col-start-3 ">
           <TopBar />
         </div>
-        <div className=" w-10/12 self-center col-start-4 col-span-9 grid grid-cols-12 ">
+        <div className=" w-10/12 self-center col-start-4 col-span-9">
           {/* <button
             className=" mb-4 self-center col-end-13 h-10 rounded-full bg-transparent border-2 border-Light-text_secondary text-Light-text_secondary  hover:border-Light-text_primary hover:text-Light-text_primary font-semibold hover:scale-105 ease-in-out duration-300"
             type="button"
@@ -28,15 +28,15 @@ function Kpi() {
           >
             SignOut
           </button> */}
-          <div className="col-span-12">
-            <TableKpi />
-          </div>
+          {/* <div className="col-span-12"> */}
+          <TableKpi />
+          {/* </div> */}
         </div>
       </div>
       {showModal ? (
         <>
           <DataEntryModal />
-          <div className=" w-40 h-40   justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none translate-x-[75rem] translate-y-[18rem] ">
+          <div className=" w-40 h-40 justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none translate-x-[75rem] translate-y-[18rem] ">
             <button
               className="inline-flex items-center justify-center w-10 h-10 mr-2 text-Light-text_secondary transition-colors duration-150 bg-Light-primary rounded-full border-2 border-Light-text_secondary text-Light-text_secondary  hover:border-Light-text_primary focus:shadow-outline hover:bg-white hover:scale-105 ease-in-out duration-300"
               // className=" px-5 m-0 self-center col-end-13 h-10 rounded-full bg-Light-primary border-2 border-Light-text_secondary text-Light-text_secondary  hover:border-Light-text_primary hover:text-Light-text_primary font-semibold hover:scale-105 ease-in-out duration-300  "
