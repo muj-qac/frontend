36ff0cb0b137889b839dbec8d615b160f13da1bd
RAz <snehalraj85@gmail.com>
2022-01-18 22:46:06 +0530

    fix:roles from array of json

---
+++ src/data/role.js
@@ -0,0 +1,23 @@
+export const role = [
+  {
+    role_name: 'admin',
+  },
+  {
+    role_name: 'dalit',
+  },
+  {
+    role_name: 'DSW',
+  },
+  {
+    role_name: 'Faculty',
+  },
+  {
+    role_name: 'HOD',
+  },
+  {
+    role_name: 'Placements',
+  },
+  {
+    role_name: 'SCIT',
+  },
+];
