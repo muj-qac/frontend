6498cc94b4f4413e99da1e351433eca220687576
RAz <snehalraj85@gmail.com>
2022-01-19 02:43:35 +0530

    feat: Login & Context fix

--- src/components/context/AuthContext.js
+++ src/components/context/AuthContext.js
@@ -30,7 +30,7 @@ export function AuthProvider({ children }) {
   console.log(children);
   return (
     <AuthContext.Provider value={value}>
-      {!loading && children}
+      {loading && children}
     </AuthContext.Provider>
   );
 }
