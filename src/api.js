4842e5957bb2512b4ce411a6512a630f7ef0f5b5
lucifer0x17 <ayush.aa347@gmail.com>
2022-05-17 16:02:07 +0530

    fix: url from local to global

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:5000/api/v1',
+  baseURL: 'http://api.mujep.in:5000/api/v1',
   withCredentials: 'true',
   headers: { 'Access-Control-Allow-Origin': 'true' },
 });
